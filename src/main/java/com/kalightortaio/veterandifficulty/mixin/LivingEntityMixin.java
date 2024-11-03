13f2558a19ac9eca7fb768a0e8d644168f9b5ef1
Kalightortaio <kokataykrishna@gmail.com>
2024-11-03 02:53:25 -0800

    implemented cave spiders

--- src/main/java/com/kalightortaio/veterandifficulty/mixin/LivingEntityMixin.java
+++ src/main/java/com/kalightortaio/veterandifficulty/mixin/LivingEntityMixin.java
@@ -10,12 +10,17 @@ import com.kalightortaio.veterandifficulty.effect.ModEffects;
 import com.kalightortaio.veterandifficulty.interfaces.IEntityState;
 import com.kalightortaio.veterandifficulty.mob.MagmaCube;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.mob.BlazeEntity;
+import net.minecraft.entity.mob.CaveSpiderEntity;
 import net.minecraft.entity.mob.DrownedEntity;
 import net.minecraft.entity.mob.EndermanEntity;
 import net.minecraft.entity.mob.GhastEntity;
@@ -35,6 +40,44 @@ import net.minecraft.world.event.GameEvent.Emitter;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
+
+    @Inject(method = "onDeath", at = @At("TAIL"))
+    private void caveSpiderDeathRattle(DamageSource source, CallbackInfo ci) {
+        if (!((Object) this instanceof CaveSpiderEntity)) return;
+        LivingEntity hurtEntity = (LivingEntity) (Object) this;
+        if (!hurtEntity.getWorld().isClient()) {
+            ServerWorld world = (ServerWorld) hurtEntity.getWorld();
+            BlockPos blockPos = hurtEntity.getBlockPos();
+            while (true) {
+                BlockState blockState = world.getBlockState(blockPos);
+                BlockState blockBelow = world.getBlockState(blockPos.down());
+                if ((blockState.isOf(Blocks.AIR) || blockState.isOf(Blocks.CAVE_AIR)) && blockBelow.isOf(Blocks.AIR)) {
+                    boolean hasAdjacentNonAir = 
+                        !world.getBlockState(blockPos.north()).isAir() ||
+                        !world.getBlockState(blockPos.south()).isAir() ||
+                        !world.getBlockState(blockPos.east()).isAir() ||
+                        !world.getBlockState(blockPos.west()).isAir() ||
+                        !world.getBlockState(blockPos.up()).isAir();
+                    if (hasAdjacentNonAir) {
+                        world.setBlockState(blockPos, Blocks.COBWEB.getDefaultState());
+                        break;
+                    }
+                    blockPos = blockPos.down();
+                } else {
+                    world.setBlockState(blockPos, Blocks.COBWEB.getDefaultState());
+                    break;
+                }
+            }
+            if (hurtEntity.getAttributeInstance(EntityAttributes.SCALE).getBaseValue() == 0.5f) return;
+            for (int i = 0; i < (2 + Math.random() * 4); i++) {
+                CaveSpiderEntity childSpider = EntityType.CAVE_SPIDER.create(world, null, blockPos, SpawnReason.TRIGGERED, false, false);
+                if (childSpider != null) {
+                    childSpider.getAttributeInstance(EntityAttributes.SCALE).setBaseValue(0.5f);
+                    world.spawnEntity(childSpider);
+                }
+            }
+        }
+    }
 
     @Inject(method = "damage", at = @At("TAIL"), cancellable = true)
     private void scaldingBlazes(ServerWorld world, DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {

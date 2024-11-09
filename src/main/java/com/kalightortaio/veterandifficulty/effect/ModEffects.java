01571d4cdaade444bc8f84d564c3d0a416462f24
Kalightortaio <kokataykrishna@gmail.com>
2024-11-08 19:20:53 -0800

    class builder for StatusEffect classes

--- src/main/java/com/kalightortaio/veterandifficulty/effect/ModEffects.java
+++ src/main/java/com/kalightortaio/veterandifficulty/effect/ModEffects.java
@@ -3,17 +3,21 @@ package com.kalightortaio.veterandifficulty.effect;
 import com.kalightortaio.veterandifficulty.VeteranDifficulty;
 
 import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.Identifier;
 
 public class ModEffects {
-    public static final StatusEffect SCALDING_EFFECT = registerEffect("scalding", new ScaldingEffect());
+    public static final StatusEffect SCALDING_EFFECT = registerEffect("scalding", StatusEffectCategory.HARMFUL, 0xcf1515);
     public static final RegistryEntry<StatusEffect> SCALDING = getRegistryEntry(SCALDING_EFFECT);
+    public static final StatusEffect ANCHORED_EFFECT = registerEffect("anchored", StatusEffectCategory.HARMFUL, 0x000080);
+    public static final RegistryEntry<StatusEffect> ANCHORED = getRegistryEntry(ANCHORED_EFFECT);
 
-    private static StatusEffect registerEffect(String path, StatusEffect effect) {
-        Registry.register(Registries.STATUS_EFFECT, Identifier.of(VeteranDifficulty.MOD_ID, path), effect);
+    private static StatusEffect registerEffect(String name, StatusEffectCategory category, int color) {
+        StatusEffect effect = new CustomStatusEffect(category, color);
+        Registry.register(Registries.STATUS_EFFECT, Identifier.of(VeteranDifficulty.MOD_ID, name), effect);
         return effect;
     }
 
@@ -23,5 +27,17 @@ public class ModEffects {
 
     public static void registerModEffects() {
         VeteranDifficulty.LOGGER.info("Registering Mod Effects for " + VeteranDifficulty.MOD_ID);
+    }
+
+    private static class CustomStatusEffect extends StatusEffect {
+
+        protected CustomStatusEffect(StatusEffectCategory category, int color) {
+            super(category, color);
+        }
+
+        @Override
+        public boolean canApplyUpdateEffect(int duration, int amplifier) {
+            return true;
+        }
     }
 }
--- src/main/java/com/kalightortaio/veterandifficulty/effect/entity/ScaldedEntity.java
+++ src/main/java/com/kalightortaio/veterandifficulty/effect/entity/ScaldedEntity.java
@@ -2,18 +2,13 @@ package com.kalightortaio.veterandifficulty.effect.entity;
 
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.kalightortaio.veterandifficulty.effect.ModEffects;
-
 import net.minecraft.entity.LivingEntity;
 
 public class ScaldedEntity {
 
     public static void onHeal(LivingEntity scalded, float health, CallbackInfo ci) {
-        if (ModEffects.SCALDING != null && scalded.hasStatusEffect(ModEffects.SCALDING)) {
-            if (health > scalded.getHealth()) {
-                ci.cancel();
-            }
+        if (health > scalded.getHealth()) {
+            ci.cancel();
         }
     }
-
 }

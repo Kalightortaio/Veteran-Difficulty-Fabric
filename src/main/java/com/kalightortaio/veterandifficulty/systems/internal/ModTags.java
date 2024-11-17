08c28256ec25ece10fecae9ade8f1b7b57284347
Kalightortaio <kokataykrishna@gmail.com>
2024-11-16 20:24:38 -0800

    added tags

--- src/main/java/com/kalightortaio/veterandifficulty/systems/internal/ModTags.java
+++ src/main/java/com/kalightortaio/veterandifficulty/systems/internal/ModTags.java
@@ -1,6 +1,7 @@
 package com.kalightortaio.veterandifficulty.systems.internal;
 
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
@@ -9,4 +10,6 @@ public class ModTags {
     public static final TagKey<Block> AIR = TagKey.of(RegistryKeys.BLOCK, Identifier.of("veterandifficulty", "air"));
     public static final TagKey<Block> MONUMENT = TagKey.of(RegistryKeys.BLOCK, Identifier.of("veterandifficulty", "monument"));
     public static final TagKey<Block> SEA_FAUNA = TagKey.of(RegistryKeys.BLOCK, Identifier.of("veterandifficulty", "sea_fauna"));
+    public static final TagKey<Block> SAPLING_SURFACE = TagKey.of(RegistryKeys.BLOCK, Identifier.of("veterandifficulty", "sapling_surface"));
+    public static final TagKey<Item> REGROWTH = TagKey.of(RegistryKeys.ITEM, Identifier.of("veterandifficulty", "regrowth"));
 }

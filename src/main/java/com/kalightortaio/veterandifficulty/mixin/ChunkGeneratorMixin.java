bb5c7ef00f850c7d9e9bef5a4f6f636c46c44abe
Kalightortaio <kokataykrishna@gmail.com>
2024-11-18 00:48:02 -0800

    implemented natural crops

--- src/main/java/com/kalightortaio/veterandifficulty/mixin/ChunkGeneratorMixin.java
+++ src/main/java/com/kalightortaio/veterandifficulty/mixin/ChunkGeneratorMixin.java
@@ -2,7 +2,9 @@ package com.kalightortaio.veterandifficulty.mixin;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.gen.StructureAccessor;
@@ -22,10 +24,40 @@ public abstract class ChunkGeneratorMixin {
             for (int z = 0; z < 16; z++) {
                 for (int y = chunk.getBottomY(); y < chunk.getTopYInclusive(); y++) {
                     BlockPos blockPos = new BlockPos(chunk.getPos().getStartX() + x, y, chunk.getPos().getStartZ() + z);
-                    BlockState blockState = chunk.getBlockState(blockPos);
+                    BlockState blockState = world.getBlockState(blockPos);
 
                     if (blockState.isOf(Blocks.TORCH) || blockState.isOf(Blocks.WALL_TORCH)) {
-                        chunk.setBlockState(blockPos, Blocks.AIR.getDefaultState(), false);
+                        world.setBlockState(blockPos, Blocks.AIR.getDefaultState(), 3);
+                    }
+
+                    if ((blockState.isOf(Blocks.SHORT_GRASS) || blockState.isOf(Blocks.FERN)) && Math.random() < 0.0002) {
+                        Box searchBox = new Box(blockPos).expand(4.0F);
+
+                        BlockPos.stream(searchBox)
+                        .filter(pos -> pos != null && world.isChunkLoaded(pos.getX() >> 4, pos.getZ() >> 4))
+                        .filter(pos -> {
+                            BlockState state = world.getBlockState(pos);
+                            return state.isOf(Blocks.SHORT_GRASS) || state.isOf(Blocks.FERN);
+                        })
+                        .forEach(pos -> {
+                            if (Math.random() < 0.55) {
+                                switch ((int) (Math.random() * 5)) {
+                                    case 0:
+                                    case 1:
+                                        world.setBlockState(pos, Blocks.WHEAT.getDefaultState().with(Properties.AGE_7, 7), 3);
+                                        break;
+                                    case 2:
+                                        world.setBlockState(pos, Blocks.CARROTS.getDefaultState().with(Properties.AGE_7, 7), 3);
+                                        break;
+                                    case 3:
+                                        world.setBlockState(pos, Blocks.POTATOES.getDefaultState().with(Properties.AGE_7, 7), 3);
+                                        break;
+                                    case 4:
+                                        world.setBlockState(pos, Blocks.BEETROOTS.getDefaultState().with(Properties.AGE_3, 3), 3);
+                                        break;
+                                }
+                            }
+                        });
                     }
                 }
             }
